5a8baaf0467f4a7c05e63fc2c078755da7a63450
blgsn <111683241+blgsn@users.noreply.github.com>
2022-09-12 12:21:03 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,245 +1,184 @@
-import React from 'react';
+mport React from 'react';
 import {
   StyleSheet, Image,
   View,
   Text,
-   useWindowDimensions,FlatList, ImageBackground
-} from 'react-native';
-
-const INTRO_DATA = [
-  {
-    key: '1',
-    img: require('./assets/image/DigitusLogo.png'),
-    
-    image: require('./assets/image/image.png'),
-    title: 'Enim ad minim',
-    title1:'Duis aute',
-    description:
-       ['Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
-        '\nsed do eiusmod tempor incididunt ut labore et dolore ' ,
-       '\nmagna aliqua. Ut enim ad minim veniam, quis nostrud',
-       '\nexercitation ullamco laboris nisi ut aliquip ex ea',
-       '\ncommodo consequat. Duis aute irure dolor in',
-       '\nreprehenderit in voluptate velit esse cillum dolore eu',   
-       '\nfugiat nulla pariatur. Excepteur sint occaecat cupidatat',
-       '\nnon proident, sunt in culpa qui officia deserunt mollit',
-       '\nanim id est laborum.',   
-       '\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit,',
-        '\nsed do eiusmod tempor incididunt ut labore et dolore',
-        '\nmagna aliqua. Ut enim ad minim veniam, quis nostrud',
-        '\nexercitation ullamco laboris nisi ut aliquip ex ea',
-         '\ncommodo consequat. Duis aute irure dolor in',
-          '\nreprehenderit in voluptate velit esse cillum dolore eu',
-          '\nfugiat nulla pariatur. Excepteur sint occaecat cupidatat',
-           '\nnon proident, sunt in culpa qui officia deserunt mollit',
-            '\nanim id est laborum.',
-          '\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit,'],
-       
-  }
-];
+ ScrollView, SafeAreaView} from 'react-native';
 
 const App = () => {
-  const { width } = useWindowDimensions();
-  const renderItem = React.useCallback( 
- 
-    ({ item }) => {
-  return (
-    <View style={[styles.itemContainer, { width: width - 80 }]}>
 
-    <Image source={item.img} style={styles.digitus}/>
-    
-    <Image source={item.image} style={styles.image3} />
-     <Text style={styles.text1}>{item.title} </Text>
-     <Text style={styles.t2}>{item.title1} </Text>
-     <Text style={styles.text}>{item.description} </Text>
-       
-    
-   
-     
-    </View>
-    
-        );
-        
-}, [width],
-  );
-  const keyExtractor = React.useCallback((item) => item.key, []);
-    
+  
   return (
-   
+   <View style={styles.container}>
     
-    <View style={[styles.container]}> 
-    <FlatList
-    data={INTRO_DATA}
-    keyExtractor={keyExtractor}
-    
-    style={styles.flatList}
-        pagingEnabled
-        vertical
-        showsVerticalScrollIndicator={false}
-        
-        decelerationRate={'normal'}
-        scrollEventThrottle={16}
-        renderItem={renderItem}
-        
-      />
-      <Image style={styles.back} source= {require('./assets/image/back1.jpg')}></Image>
+      <Image source={ require("./assets/image/Rectangle.png")} style={styles.rectangle}/>
+        <Image style={styles.back} source= {require('./assets/image/back.png')}></Image>
       <Image style={styles.not} source={require('./assets/image/not.png')} ></Image> 
+      
+      
+       <SafeAreaView><ScrollView> 
+       
+        <Image style={styles.digitus} source={require('./assets/image/DigitusLogo.png')}/>
+       
+        <Image source={require('./assets/image/image.png')} style={styles.image1} />
+     <Text style={styles.text1}>Enim ad minim </Text>
+     <Text style={styles.t2}>Duis aute</Text>
+     <Text style={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+     {'\n'}sed do eiusmod tempor incididunt ut labore et dolore
+    {'\n'}magna aliqua. Ut enim ad minim veniam, quis nostrud
+    {'\n'}exercitation ullamco laboris nisi ut aliquip ex ea
+    {'\n'}commodo consequat. Duis aute irure dolor in
+    {'\n'}reprehenderit in voluptate velit esse cillum dolore eu  
+    {'\n'}fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+    {'\n'}non proident, sunt in culpa qui officia deserunt mollit
+    {'\n'}anim id est laborum.  
+    {'\n\n'}Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+     {'\n'}sed do eiusmod tempor incididunt ut labore et dolore
+     {'\n'}magna aliqua. Ut enim ad minim veniam, quis nostrud
+     {'\n'}exercitation ullamco laboris nisi ut aliquip ex ea
+      {'\n'}commodo consequat. Duis aute irure dolor in
+       {'\n'}reprehenderit in voluptate velit esse cillum dolore eu
+       {'\n'}fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+        {'\n'}non proident, sunt in culpa qui officia deserunt mollit
+         {'\n'}anim id est laborum.
+       {'\n\n'}Lorem ipsum dolor sit amet, consectetur adipiscing elit, </Text>
+      </ScrollView></SafeAreaView>
       </View>
-    );}
-
+  );
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-   marginBottom:170,
-   marginTop:-215,
-paddingBottom:10,
-paddingTop:10,
-    alignItems: 'center',
- 
-    justifyContent: 'center',
+  background:{
+  
+    width:300,
+    heigth:80,
+    backgroundColor:'#c9caca',
+    
+    top:50,
   },
-  image3: {
+  container: {
+    flex: 10,
+    backgroundColor: 'black',
+   marginTop:-115,
+   marginBottom:0,
+   paddingBottom:85,
+   
+  },
+  image1: {
     flex: 5,
     width:410,
     height: 325,
     margin:10,
-    marginTop: -25,
-    marginLeft:-35,
-    top:-125,
+    top:116,
     marginBottom: 95,
-    left:20,
+    left:-10,
     resizeMode: 'contain',
+    zIndex:-1,
     
   },
   back: {
-    flex:2,
-    width:415,
+    flex:1,
+    width:25,
     marginTop:340,
     marginBottom:-230,
-    height: 50,
+    height: 5,
     bottom:0,
     right:0,
-    top:-45,
-    paddingBottom:20,
+    top:-115,
+    paddingBottom:10,
     paddingTop:10,
-    left:0,
-    zIndex:-1,
+    left:20,
     position:'absolute',
-    
-    
-    
-    
-  
+    zIndex:1,
 },
-
   digitus: {
     width: 115,
     height: 60,
-    margin:10,
-    marginTop: -255,
-    marginBottom: 200,
-    marginLeft: 40,
-   top:50,
-   left:60,
+    top:65,
+    left:140,
     resizeMode: 'cover',
     position:'absolute',
     
-    zIndex:1,
-    
-    
-    
   },
+  rectangle: {
+    width: 435,
+    height: 100,
+    bottom:720,
+    resizeMode: 'cover',
+    position:'absolute',
+    zIndex:0,
+    },
   text: {
-    
+    zIndex:-2,
     flex: 20,
-    margin:10,
-    marginTop: 8,
-    marginBottom: 10,
-    marginLeft:10,
-    marginRight:-40,
-    
-    left:-22,
+    backgroundColor: 'white',
+    borderBottomWidth: 2,
+    borderTopWidth:2,
+    borderLeftWidth: 20,
+    borderRightWidth: 2,
     fontSize: 14,
     lineHeight: 21,
     color: '#050505',
-    top:-30,
+    top:200,
     justifyContent: 'center',
-    
+     marginRight:-10,
+     marginLeft:1,
+   left:-1,
+   paddingLeft:10,
+   paddingRight:5,
+   
   },
   text1: {
-    flex: 4,
+    flex: 5,
     borderRadius:25,
+    zIndex:-1,
     backgroundColor: 'white',
-    borderBottomWidth: 40,
-    borderTopWidth:40,
+    borderBottomWidth: 75,
+    borderTopWidth:30,
     borderLeftWidth: 30,
-    borderRightWidth: 220,
-    margin:1,
+    borderRightWidth: 180,
     marginTop: -125,
-    marginBottom:-130,
-   marginLeft:-94,
-   marginRight:-50,
-   paddingLeft:-10,
-   paddingRight:-5,
-  left:20,
+    marginBottom:-145,
     fontSize: 23,
-    top:-128,
+    top:121,
     color: '#2f662c',
     fontWeight: 'bold',
-    
     overflow: 'hidden',
 },
 t2: {
-  margin:10,
+  margin:5,
   marginTop: 15, 
   marginBottom: 10,
   flex: 10,
-    borderRadius:10,
+    borderRadius:12,
     borderColor: '#c3d8c8',
     borderWidth:1.2,
    borderRightWidth:10,
    borderLeftWidth:15,
-    paddingLeft:10,
+    paddingLeft:12,
     paddingTop:2,
     paddingBottom:2,
-    paddingRight:30,
-    marginRight:25,
+    paddingRight:20,
+    marginRight:240,
     marginLeft:60,
-    top:-35,
-    left:-135,
+    top:192,
+    left:-30,
     fontSize: 12,
-    
+    zIndex:-1,
     color: '#2d703c',
     alignContent:'center',
     justifyContent: 'center',
     overflow: 'hidden',
 },
 not: {
-  flex:1,
-  width:410,
-    height: 70,
-marginTop:-90,
-    top:35,
-    bottom:20,
-    marginBottom:-50,
-    right:0,
-    left: -5,
- 
-  
+  flex:2,
+  width:435,
+    height: 50,
+  resizeMode:'contain',
+    top:828,
+    zIndex:1,
+    left:-10,
 },
-itemContainer: {
-  backgroundColor: '#fff',
-  justifyContent: 'center',
-  alignItems: 'center',
-  
-  marginTop:330,
-  marginBottom:150,
-  bottom:-150,
-  marginHorizontal: 40,
-  zIndex:-2,
-  borderRadius: 20,
-}
+
 });
 export default App;
